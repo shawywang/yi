82b1c2e92d9d83e7a707c7740e9e08ece561ddd4
wangxiao <shaw.y.wang@foxmail.com>
2025-11-08 12:36:23 +0800

    加备注

--- 字根画26键.py
+++ 字根画26键.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 key_images: str = "qwertyuiopasdfghjklzxcvbnm"
 
@@ -38,6 +38,18 @@ class Handle:
                 x_offset += (key_width + padding)
             y_offset += (key_height + padding)
 
+        # 宽1439，高484
+        # 加备注文字
+        n_x = round(key_width * 1.2) + 7 * (key_width + padding)
+        n_y = 2 * (key_height + padding)
+        n_font1 = ImageFont.truetype(font=r"C:\Users\wangxiao\AppData\Local\Microsoft\Windows\Fonts\华文楷体粗.ttf", size=25)
+        n_font2 = ImageFont.truetype(font=r"C:\Users\wangxiao\AppData\Local\Microsoft\Windows\Fonts\IntelOneMono-Bold.ttf", size=20)
+
+        draw.text(xy=(n_x, n_y), text="注：因无字根字\n红色“落”应去掉“各”、\n“释”应去掉右边；\n红色“一丨丶丿”为首笔", fill="black", font=n_font1)
+        draw.text(xy=(n_x, n_y + 120), text="github.com/shawywang/yi", fill=(0, 0, 0), font=n_font2)
+        draw.text(xy=(n_x, n_y + 140), text="or: gitee.com/shawywang/yi", fill=(95, 95, 95), font=n_font2)
+        draw.text(xy=(n_x, n_y + 160), text="QQ: 790835977", fill=(0, 0, 0), font=n_font2)
+
         # 保存
         palette_img = Image.new("P", (1, 1))  # 调色板
         palette_img.putpalette(
@@ -52,7 +64,7 @@ class Handle:
         )
         fixed_image = keyboard_image.quantize(palette=palette_img, dither=Image.Dither.NONE)
         fixed_image.save(
-            fp="C:\\Users\\wangxiao\\Downloads\\keyboard.webp",
+            fp="C:\\Users\\wangxiao\\Downloads\\字根图（PIL生成）.webp",
             optimize=True,
             compress_level=5,
             dpi=(72, 72),
